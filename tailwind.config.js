7e1bb1cbddfaa6800168e30272ba8af3f15e5c28
atoilahputra016 <48822156+atoilahputra016@users.noreply.github.com>
2022-08-19 18:13:02 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,js}'],
+  content: ['index.html'],
   theme: {
     container : {
       center: true,
